ce953a44f10a74151a1300f78896c67d06265472
Red16 <71528727+Rednick16@users.noreply.github.com>
2022-08-08 20:46:26 -0500

    Alignment fix

--- monoArray.h
+++ monoArray.h
@@ -1,25 +1,23 @@
 template <typename T>
 struct monoArray
 {
-    void* klass;
-    void* monitor;
-    void* bounds;
-    int   max_length;
-    void* vector [1];
-    int getLength()
-    {
-        return max_length;
-    }
-    T getPointer()
-    {
-        return (T)vector;
-    }
-  
-  	TValue operator [] (int i) {
-		  return getPointer()[i];
-	  }
-
-	  const TValue operator [] (int i) const {
-		  return getPointer()[i];
-	  }
+	void* klass;
+	void* monitor;
+	void* bounds;
+	int   max_length;
+	void* vector [1];
+	int getLength()
+	{
+	    return max_length;
+	}
+	T getPointer()
+	{
+	    return (T)vector;
+	}	
+	TValue operator [] (int i) {
+		return getPointer()[i];
+	}	
+	const TValue operator [] (int i) const {
+		return getPointer()[i];
+	}
 };
