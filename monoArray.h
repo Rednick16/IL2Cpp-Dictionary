95b23e0ef177084019238403073f1b7b0fd9991f
Red16 <71528727+Rednick16@users.noreply.github.com>
2022-08-10 02:46:44 -0500

    added some new features

--- monoArray.h
+++ monoArray.h
@@ -6,18 +6,29 @@ struct monoArray
 	void* bounds;
 	int   max_length;
 	void* vector [1];
+
 	int getLength()
 	{
-	    return max_length;
+		return max_length;
 	}
+
 	T getPointer()
 	{
-	    return (T)vector;
-	}	
+		return (T)vector;
+	}
+
 	T operator [] (int i) {
 		return getPointer()[i];
-	}	
+	}
+
 	const T operator [] (int i) const {
 		return getPointer()[i];
+	}
+
+	bool Contains(T item) {
+		for (int i = 0; i < max_length; i++){
+			if(getPointer()[i] == item) return true;
+		}
+		return false;
 	}
 };
