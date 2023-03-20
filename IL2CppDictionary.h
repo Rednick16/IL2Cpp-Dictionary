a600921c1e5c140fb533dff7a22d0eeb6b5a7e82
Red16 <71528727+Rednick16@users.noreply.github.com>
2023-03-20 05:47:13 -0500

    Update IL2CppDictionary.h

--- IL2CppDictionary.h
+++ IL2CppDictionary.h
@@ -1,123 +1,167 @@
 #include "monoArray.h"
 
 template<typename TKey, typename TValue> 
-struct Dictionary {
-struct KeysCollection;
-struct ValueCollection;
+struct Dictionary 
+{
+    struct KeysCollection;
+    struct ValueCollection;
 
-	struct Entry
-	{
-		int hashCode; 
-		int next;
-		TKey key;
-		TValue value;
-	};
+    struct Entry
+    {
+        int hashCode; 
+        int next;
+        TKey key;
+        TValue value;
+    };
 
-	void *kass;
-	void *monitor;
-	monoArray<int *> *buckets;
-	monoArray<Entry *> *entries;
-	int count;
-	int version;
-	int freeList;
-	int freeCount;
-	void* comparer;
-	KeysCollection *keys;
-	ValueCollection *values;
-	void *_syncRoot;
+    void *kass;
+    void *monitor;
+    Array<int> *buckets;
+    Array<Entry> *entries;
+    int count;
+    int version;
+    int freeList;
+    int freeCount;
+    void* comparer;
+    KeysCollection *keys;
+    ValueCollection *values;
+    void *_syncRoot;
 
-	void* get_Comparer(){
-		return comparer;
-	}
+    void* get_Comparer()
+    {
+        return comparer;
+    }
 
-	int get_Count(){
-		return (count - freeCount);
-	}
+    int get_Count()
+    {
+        return (count - freeCount);
+    }
 
-	KeysCollection get_Keys(){
-		if(!keys){
-			keys = new KeysCollection;
-			keys->dictionary = this;
-		}
-		return (*keys);
-	}
+    KeysCollection get_Keys()
+    {
+        if(!keys) keys = new KeysCollection(this);
+        return (*keys);
+    }
 
-	ValueCollection get_Values(){
-		if(!values){
-			values = new ValueCollection;
-			values->dictionary = this;
-		}
-		return (*values);
-	}
+    ValueCollection get_Values()
+    {
+        if(!values) values = new ValueCollection(this);
+        return (*values);
+    }
 
-	TValue operator [] (TKey key) {
-		int i = FindEntry(key);
-        if (i >= 0) return entries->getPointer()[i].value;
-		return TValue();
-	}
+    TValue operator [] (TKey key) 
+    {
+        int i = FindEntry(key);
+        if (i >= 0) return (*entries)[i].value;
+        return TValue();
+    }
 
-	const TValue operator [] (TKey key) const {
-		int i = FindEntry(key);
-        if (i >= 0) return entries->getPointer()[i].value;
-		return TValue();
-	}
-	
-	int FindEntry(TKey key) {
-		for (int i = 0; i < count; i++){
-			if(entries->getPointer()[i].key == key) return i;
-		}
-	    return -1;
-	}
-	
-	bool ContainsKey(TKey key) {
-		return FindEntry(key) >= 0;
-	}
-	
-	bool ContainsValue(TValue value) {
-		for (int i = 0; i < count; i++){
-			if(entries->getPointer()[i].hashCode >= 0 && 
-					entries->getPointer()[i].value == value) return true;
-		}
-	    return false;
-	}
+    const TValue operator [] (TKey key) const 
+    {
+        int i = FindEntry(key);
+        if (i >= 0) return (*entries)[i].value;
+        return TValue();
+    }
+    
+    int FindEntry(TKey key) 
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if((*entries)[i].key == key) return i;
+        }
+        return -1;
+    }
+    
+    bool ContainsKey(TKey key) 
+    {
+        return FindEntry(key) >= 0;
+    }
+    
+    bool ContainsValue(TValue value) 
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if((*entries)[i].hashCode >= 0 && 
+                    (*entries)[i].value == value) return true;
+        }
+        return false;
+    }
 
-	struct KeysCollection {
-		void *kass;
-		void *monitor;
-		Dictionary<TKey, TValue> *dictionary;
+    bool TryGetValue(TKey key, TValue *value) 
+    {
+        int i = FindEntry(key);
+        if (i >= 0) {
+            *value = (*entries)[i].value;
+            return true;
+        }
+        *value = TValue();
+        return false;
+    }
 
-		TKey operator [] (int i) {
-			if(dictionary->entries)return dictionary->entries->getPointer()[i].key;
-			return TKey();
-		}
+    TValue GetValueOrDefault(TKey key) 
+    {
+        int i = FindEntry(key);
+        if (i >= 0) {
+            return (*entries)[i].value;
+        }
+        return TValue();
+    }
 
-		const TKey operator [] (int i) const {
-			if(dictionary->entries)return dictionary->entries->getPointer()[i].key;
-			return TKey();
-		}
+    struct KeysCollection 
+    {
+        Dictionary *dictionary;
 
-		int get_Count(){
-			return dictionary->count();
-		}
-	};
+        KeysCollection(Dictionary *dictionary)
+        {
+            this->dictionary = dictionary;
+        }
 
-	struct ValueCollection {
-		void *kass;
-		void *monitor;
-		Dictionary<TKey, TValue> *dictionary;
+        TKey operator [] (int i) 
+        {
+            auto entries = dictionary->entries;
+            if(!entries) return TKey();
+            return (*entries)[i].key;
+        }
 
-		TValue operator [] (int i) {
-			if(dictionary->entries)return dictionary->entries->getPointer()[i].value;
-			return TValue();
-		}
+        const TKey operator [] (int i) const 
+        {
+            auto entries = dictionary->entries;
+            if(!entries) return TKey();
+            return (*entries)[i].key;
+        }
 
-		const TValue operator [] (int i) const {
-			if(dictionary->entries)return dictionary->entries->getPointer()[i].value;
-			return TValue();
-		}
+        int get_Count()
+        {
+            return dictionary->get_Count();
+        }
+    };
 
-		int get_Count(){
-			return dictionary->count();
-		}
-	};
+    struct ValueCollection 
+    {
+        Dictionary *dictionary;
+
+        ValueCollection(Dictionary *dictionary)
+        {
+            this->dictionary = dictionary;
+        }
+
+        TValue operator [] (int i) 
+        {
+            auto entries = dictionary->entries;
+            if(!entries) return TValue();
+            return (*entries)[i].value;
+        }
+
+        const TValue operator [] (int i) const 
+        {
+            auto entries = dictionary->entries;
+            if(!entries) return TValue();
+            return (*entries)[i].value;
+        }
+
+        int get_Count()
+        {
+            return dictionary->get_Count();
+        }
+    };
 };
